0b56e8bebcd58bf592f239337461c29e324e7d48
regression-test-executor <regression-test-executor@speakeasy.com>
2024-10-07 14:23:38 +0000

    openapi-generation@f46d6482d182cdfdeefc1072b81e06d8bd08b169

--- lib/open_api_sdk/sdkconfiguration.rb
+++ lib/open_api_sdk/sdkconfiguration.rb
@@ -39,8 +39,8 @@ module OpenApiSDK
       @language = 'ruby'
       @openapi_doc_version = '0.0.1'
       @sdk_version = '0.2.2-alpha.23'
-      @gen_version = '2.434.1'
-      @user_agent = 'speakeasy-sdk/ruby 0.2.2-alpha.23 2.434.1 0.0.1 dub'
+      @gen_version = '2.434.4'
+      @user_agent = 'speakeasy-sdk/ruby 0.2.2-alpha.23 2.434.4 0.0.1 dub'
     end
 
     sig { returns([String, T::Hash[Symbol, String]]) }
